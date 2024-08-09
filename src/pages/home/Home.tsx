91b7cc1f392fe4243305c6e791ad7e979bc3b4eb
Kokinho00 <flaviocoutinho@hotmail.com>
2024-08-08 21:52:07 -0300

    login

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
+import React, { useContext } from 'react';
 
+import { UserContext } from '../../contexts/UserContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
+  const { nome, setNome } = useContext(UserContext);
     return (
         <>
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
   
               <div className="flex justify-around gap-4">
               
