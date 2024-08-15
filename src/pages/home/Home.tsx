1b7109d5654d26144020f05738cb6179360b68bf
Kokinho00 <flaviocoutinho@hotmail.com>
2024-08-15 12:43:58 -0300

    postagens crud

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,17 @@
+import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
-import React, { useContext } from 'react';
+import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens'
 
-import { UserContext } from '../../contexts/UserContext';
-import { Link, useNavigate } from 'react-router-dom';
 
 function Home() {
-  const { nome, setNome } = useContext(UserContext);
     return (
         <>
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
               
@@ -27,7 +25,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        <ListaPostagens />
       </>
     );
 }
